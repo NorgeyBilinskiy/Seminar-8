62c132b24b13c00461b8a13ba3054f7a80161a2a
Liandon <bilinskiy-01@mail.ru>
2023-01-05 22:27:03 +0600

    4

--- homtwork/task60/Program.cs
+++ homtwork/task60/Program.cs
@@ -1,67 +1,74 @@
 ﻿// Сформируйте трёхмерный массив из неповторяющихся (HARD - случайных уникальных) двузначных чисел. 
 // Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 
-void FillArray(int[,,] array3D)
+void FillArray(int[,,] array, int min = 10, int max = 100)
 {
-  int[] temp = new int[array3D.GetLength(0) * array3D.GetLength(1) * array3D.GetLength(2)];
-  int  number;
-  for (int i = 0; i < temp.GetLength(0); i++)
-  {
-    temp[i] = new Random().Next(10, 100);
-    number = temp[i];
-    if (i >= 1)
+    Random rand = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-      for (int j = 0; j < i; j++)
-      {
-        while (temp[i] == temp[j])
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-          temp[i] = new Random().Next(10, 100);
-          j = 0;
-          number = temp[i];
+            for (int h = 0; h < array.GetLength(2); h++)
+            {
+                while (array[i, j, h] == 0)
+                {
+                    int number = rand.Next(min, max);
+
+                    if (NumberArray(array, number) == false)
+                    {
+                        array[i, j, h] = number;
+                    }
+                }
+
+            }
         }
-          number = temp[i];
-      }
     }
-  }
-  int count = 0; 
-  for (int x = 0; x < array3D.GetLength(0); x++)
-  {
-    for (int y = 0; y < array3D.GetLength(1); y++)
-    {
-      for (int z = 0; z < array3D.GetLength(2); z++)
-      {
-        array3D[x, y, z] = temp[count];
-        count++;
-      }
-    }
-  }
 }
 
-void PrintArray (int[,,] array3D)
+bool NumberArray(int[,,] array, int number)
 {
-  for (int i = 0; i < array3D.GetLength(0); i++)
-  {
-    for (int j = 0; j < array3D.GetLength(1); j++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-      Console.Write($"X({i}) Y({j}) ");
-      for (int k = 0; k < array3D.GetLength(2); k++)
-      {
-        Console.Write( $"Z={array3D[i,j,k]}; ");
-      }
-      Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int h = 0; h < array.GetLength(2); h++)
+            {
+                if (array[i, j, h] == number) return true;
+            }
+        }
     }
-    Console.WriteLine();
-  }
+
+    return false;
 }
 
-Console.WriteLine($"Введите размер массива X,Y,Z: ");
-Console.WriteLine("Введите X: ");
-int x = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите Y: ");
-int y = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите Z: ");
-int z = Convert.ToInt32(Console.ReadLine());
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int h = 0; h < array.GetLength(2); h++)
+            {
+                Console.Write(array[i, j, h]);
+                Console.Write("({0},{1},{2})\t", i, j, h);
+            }
 
-int[,,] array3D = new int[x, y, z];
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Добрый день!");
+Console.WriteLine("Массив имеет размер 2*2*2");
+Console.WriteLine("Значения в массиве изменяются от 10 до 99 включительно");
+
+int SizeX = 2;
+int SizeY = 2;
+int SizeZ = 2;
+int[,,] array3D = new int[SizeX,SizeY,SizeZ];
+
 FillArray(array3D);
 PrintArray(array3D);
